1b36eb93632d40bc129352b4ac65c532ec2bac31
Farook Khan <farook@Farooks-MacBook-Air.local>
2017-06-03 19:48:11 -0700

    Added npm script to run test on start and build. #16

--- src/client/features/accepted/Accepted.test.js
+++ src/client/features/accepted/Accepted.test.js
@@ -5,7 +5,7 @@ import {Accepted} from './Accepted';
 describe('<Accepted />', () => {
   it('should render a div', () => {
     const wrapper = shallow(<Accepted />);
-    expect(wrapper.find("div").length).toBe(1)
-    expect(wrapper.find("AcceptedTable").length).toBe(1)
-  })
+    expect(wrapper.find("div").length).toBe(1);
+    expect(wrapper.find("AcceptedTable").length).toBe(1);
+  });
 });
