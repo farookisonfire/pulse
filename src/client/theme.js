8750b08f0fc01306d074ce98da34486bdc51654f
Farook Khan <farook.khan@gmail.com>
2017-08-29 00:03:43 -0700

    Implement dark theme and css resets.

---
+++ src/client/theme.js
@@ -0,0 +1,31 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {
+  cyan500,
+  cyan700,
+  red200,
+  red400,
+  grey100,
+  grey400,
+  grey500,
+  grey800,
+  white,
+  darkBlack,
+  fullBlack,   
+} from 'material-ui/styles/colors';
+
+export const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: cyan500,
+    primary2Color: cyan700,
+    primary3Color: grey400,
+    accent1Color: red400,
+    accent2Color: red200,
+    accent3Color: grey500,
+    textColor: white,
+    alternateTextColor: darkBlack,
+    canvasColor: grey800,
+    borderColor: grey500,
+    pickerHeaderColor: cyan500,
+    shadowColor: fullBlack,
+  },
+});
